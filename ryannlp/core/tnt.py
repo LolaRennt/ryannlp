a6b032001569c9d483178c8561a65f1c44e1cafc
8326369 <ryanliu1991@gmail.com>
2014-03-10 16:00:39 +0800

    Tnt beta 0.1

--- ryannlp/core/tnt.py
+++ ryannlp/core/tnt.py
@@ -51,11 +51,11 @@ class TnT(object):
 
         for item in self.tri.allItems():
 
-            case3 = 0 if (self.big.getCount(item[:2]) - 1 ) == 0 else (self.tri.getCount(item) - 1) / (self.big.getCount(item[:2]) - 1)
+            case3 = 0 if (self.big.getCount(item[:2]) - 1 ) == 0 else (self.tri.getCount(item) - 1)     / (self.big.getCount(item[:2]) - 1)
 
             case2 = 0 if (self.uni.getCount(item[1]) - 1)   == 0 else (self.big.getCount(item[1:]) -1 ) / (self.uni.getCount(item[1]) - 1)
 
-            case1 = 0 if (self.uni.getTotle() - 1)          == 0 else (self.uni.getCount(item[2]) -1 ) / (self.uni.getTotle() - 1)
+            case1 = 0 if (self.uni.getTotle() - 1)          == 0 else (self.uni.getCount(item[2]) -1 )  / (self.uni.getTotle() - 1)
 
 
             if case3 >= case2 and case3 >= case1:
@@ -73,48 +73,44 @@ class TnT(object):
 
         print self.lam1,self.lam2,self.lam3
 
-
         for s1 in self.state:
             for s2 in self.state:
                 for s3 in self.state:
                     if s3 != u'P':
                         p1 = self.lam1 * self.uni.getCount(s3) / self.uni.getTotle()
-                        if self.uni.getCount(s2) == 0:
-                            p2 = 0
-                        else:
-                            p2 = self.lam2 * self.big.getCount((s2,s3)) / self.uni.getCount(s2)
-                        if self.big.getCount((s1,s2)) == 0:
-                            p3 = 0
-                        else:
-                            p3 = self.lam3 * self.tri.getCount((s1,s2,s3)) / self.big.getCount((s1,s2))
-                        
-                        try:
-                            self.trans[(s1,s2,s3)] = math.log(p1+p2+p3)
-                        except:
-                            pass
-                            
-        
+                        p2 = 0 if self.uni.getCount(s2)      == 0 else self.lam2 * self.big.getCount((s2,s3)) / self.uni.getCount(s2)
+                        p3 = 0 if self.big.getCount((s1,s2)) == 0 else self.lam3 * self.tri.getCount((s1,s2,s3)) / self.big.getCount((s1,s2))
+
+                        self.trans[(s1,s2,s3)] = math.log(p1+p2+p3)
+
+
     def tag(self,sentence):
         sentence.append(u'EOF')
         tagnow = [[(u'P',u'P'),0.0]]
         for word in sentence:
+            #print word
             stateset = self.words.get(word,self.state)
-            print stateset
+            #print stateset
             tagpre = tagnow[:]
             tagnow = []
             for state in stateset:
                 if state == u'P':
                     continue
+                #print state
                 tagtemp = []
                 for item in tagpre:
                     tritutle = (item[0][-2],item[0][-1],state)
+
                     probnow = item[1]
                     probnow += self.trans[tritutle]
                     probnow += math.log(self.wd.getCount((word,state))) - math.log(self.uni.getCount(state))
-                    tagtemp.append([tritutle,probnow])
 
+                    statenow = list(item[0])
+                    statenow.append(state)
+
+                    tagtemp.append([tuple(statenow),probnow])
+                #print tagtemp
                 findmax = max(tagtemp,key=lambda x:x[1])
                 tagnow.append(findmax)
-
         return tagnow
 
